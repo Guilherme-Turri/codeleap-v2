80219aa8a46818090148def21a472bc6c2710b17
Guilherme Turri <guilhermeturri@icloud.com>
2023-05-08 12:41:05 -0300

    test[FormLogin]:add test when is loading

--- src/templates/FormLogin/FormLogin.test.tsx
+++ src/templates/FormLogin/FormLogin.test.tsx
@@ -3,25 +3,31 @@ import { FormLogin } from '.'
 import { screen } from '@testing-library/react'
 
 describe('<FormLogin/>', () => {
+  const propsFormLogin = {
+    usermail: {
+      onChange: jest.fn(),
+      value: '123',
+      error: 'Msgerror'
+    },
+    userpassword: {
+      onChange: jest.fn(),
+      value: '123',
+      error: 'Msgerror'
+    },
+    handleLogin: jest.fn(),
+    loading: false
+  }
   it('should render on screen properly', () => {
-    const propsFormLogin = {
-      usermail: {
-        onChange: jest.fn(),
-        value: '123',
-        error: 'Msgerror'
-      },
-      userpassword: {
-        onChange: jest.fn(),
-        value: '123',
-        error: 'Msgerror'
-      },
-      handleLogin: jest.fn(),
-    }
-
     componentRender(<FormLogin {...propsFormLogin} />)
     expect(screen.getByText('Enter')).toBeInTheDocument();
     expect(screen.getByTestId('form')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+  })
+  it('should render on screen with Loading text', () => {
+    propsFormLogin.loading = true;
+    const { container } = componentRender(<FormLogin {...propsFormLogin} />)
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    screen.debug(container)
   })
 })
